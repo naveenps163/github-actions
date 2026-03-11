8a2f2487c920b0c5863b5457f00309653878a4e2
naveenps <naveenps164@gmail.com>
2026-03-11 12:10:06 +0530

    updated files

--- project/main.tf
+++ project/main.tf
@@ -1,8 +1,15 @@
+variable "instance_count" {
+  default = 1
+}
+
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0f58b397bc5c1f2e8"
+
+  count = var.instance_count
+
+  ami           = "ami-03f4878755434977f"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "github-actions-ec2"
+    Name = "github-actions-ec2-${count.index}"
   }
 }
