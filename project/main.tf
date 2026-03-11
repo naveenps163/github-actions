ba29ccd2980e34d0fa97b5a156518a37179c2af9
naveenps <naveenps164@gmail.com>
2026-03-11 11:24:37 +0530

    updated code

--- project/main.tf
+++ project/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "github-actions-ec2"
